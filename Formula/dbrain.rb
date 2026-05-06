1f489993c0878f17592c090e818fd74dffce53a2
Darron Froese <darron@froese.org>
2026-05-05 21:12:15 -0600

    Fix dbrain platform URL selection

--- Formula/dbrain.rb
+++ Formula/dbrain.rb
@@ -9,25 +9,19 @@ class Dbrain < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
-  on_macos do
-    on_arm do
+  if OS.mac?
+    if Hardware::CPU.arm?
       url "https://github.com/darron/dbrain/releases/download/v#{version}/dbrain_v#{version}_darwin_arm64.tar.gz"
       sha256 "5e7efc7b2766c6e8482dbbf149aff715b6c21a848404d92cc7d2c656835c55e3"
-    end
-
-    on_intel do
+    else
       url "https://github.com/darron/dbrain/releases/download/v#{version}/dbrain_v#{version}_darwin_amd64.tar.gz"
       sha256 "6f751f975a20f801ce7596ffc4ec1016f5ee916946f8e423a73b8818fc0c89d6"
     end
-  end
-
-  on_linux do
-    on_arm do
+  elsif OS.linux?
+    if Hardware::CPU.arm?
       url "https://github.com/darron/dbrain/releases/download/v#{version}/dbrain_v#{version}_linux_arm64.tar.gz"
       sha256 "3c870995f622283979b0a3ebc1787f4e9b43bc364140a71ef89e4ed982879f26"
-    end
-
-    on_intel do
+    else
       url "https://github.com/darron/dbrain/releases/download/v#{version}/dbrain_v#{version}_linux_amd64.tar.gz"
       sha256 "86098ac14a753ec65e9c0db31077a1d58aa7ce34f5a8c03eae00e0c9f9f352c7"
     end
